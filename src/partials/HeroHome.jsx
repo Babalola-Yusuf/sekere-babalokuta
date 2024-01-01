7dea6fc00e19620d24921dd1d4dce1e3d41a4134
Babalola-Yusuf <yusufbabalola01@gmail.com>
2024-01-01 05:48:51 -0800

    text added to HeroHome and Testimonials

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -36,7 +36,7 @@ function HeroHome() {
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-4xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-              Welcome to <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400  text-4xl sm:text-5xl md:text-6xl">Babalokuta/Sekere</span>
+              Welcome to <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400  text-4xl sm:text-5xl md:text-6xl">Babalokuta/Sekere</span> New Town
             </h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
